a64f7cdc4f3c7547fa74f5250f8bb32f464f8efe
Jodenee <81998397+Jodenee@users.noreply.github.com>
2025-08-20 17:05:58 +0200

    update console.py
    i'm honestly not sure if i should be proud or ashamed of that tuple unpacking

--- src/core/utilities/console.py
+++ src/core/utilities/console.py
@@ -1,4 +1,4 @@
 from typing import Generic, Optional, Tuple, TypeVar
 
 def spaced_print(*objects, sep: Optional[str] = None, flush: bool = False) -> None:
-    print(*objects, end="\n\n", sep=sep, flush=flush)
+    print(*("\n", *objects), sep=sep, flush=flush)
