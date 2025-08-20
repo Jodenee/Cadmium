df607f4bc8c9bde4fe8b0e27a6c5cc924c16344c
Jodenee <81998397+Jodenee@users.noreply.github.com>
2025-08-20 16:45:26 +0200

    added ui_configuration.py

---
+++ src/core/custom_types/ui_configuration.py
@@ -0,0 +1,7 @@
+from typing import Optional, TypedDict
+
+class UIConfiguration(TypedDict):
+    custom_download_bar_colour: Optional[str]
+    custom_convert_bar_colour: Optional[str]
+    custom_clear_directory_bar_colour: Optional[str]
+    
