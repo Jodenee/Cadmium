b0629904f5f0679af0aab6e96b6efdbaeed177ad
Jodenee <81998397+Jodenee@users.noreply.github.com>
2025-08-20 16:44:14 +0200

    update configuration.py
    add new setting group ui_configuration

--- src/core/custom_types/configuration.py
+++ src/core/custom_types/configuration.py
@@ -3,8 +3,10 @@ from typing import TypedDict
 from .quality_of_life_configuration import QualityOfLifeConfiguration
 from .warning_configuration import WarningConfiguration
 from .download_behavior_configuration import DownloadBehaviorConfiguration
+from .ui_configuration import UIConfiguration
 
 class Configuration(TypedDict):
     download_behavior_configuration: DownloadBehaviorConfiguration
     quality_of_life_configuration: QualityOfLifeConfiguration
     warning_configuration: WarningConfiguration
+    ui_configuration: UIConfiguration
