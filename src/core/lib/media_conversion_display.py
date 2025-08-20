878d1634d56df6f628b9b6ee9b6680800c19959e
Jodenee <81998397+Jodenee@users.noreply.github.com>
2025-08-20 17:00:33 +0200

    update media_conversion_display.py
    redid the way descriptions are passed, custom coloursgit add src/core/lib/media_conversion_display.py !

--- src/core/lib/media_conversion_display.py
+++ src/core/lib/media_conversion_display.py
@@ -1,17 +1,22 @@
 from typing import Optional
 
-from pytubefix import Stream
-from tqdm import tqdm
+from pytubefix.async_youtube import Stream
+from tqdm.asyncio import tqdm
 from ffmpeg import FFmpeg, Progress
+
+from core.custom_types import Configuration
 
 from ..enums import Colours
 
 class MediaConversionDisplay:
-    def __init__(self, stream: Stream, ffmpeg: FFmpeg, custom_description: Optional[str] = None) -> None:
+    def __init__(self, description: str, stream: Stream, ffmpeg: FFmpeg, configuration: Configuration) -> None:
+        custom_colour = configuration["ui_configuration"]["custom_convert_bar_colour"]
+
+        print() # print new line before progress bar
         self.progress_bar: tqdm = tqdm(
-            desc=f"Converting ({stream.title})" if custom_description == None else custom_description, 
+            desc=description,
             total=round(int(stream.durationMs) / 1000, 2), 
-            colour=Colours.CADMIUM_ORANGE.value, 
+            colour=custom_colour if custom_colour and len(custom_colour) > 0 else Colours.CADMIUM_ORANGE.value, 
             unit="seconds"
         )
         self.ffmpeg = ffmpeg
