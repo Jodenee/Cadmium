cba07f8718036fe9a26fa4477d29a5c36c84b0e8
Jodenee <81998397+Jodenee@users.noreply.github.com>
2025-08-24 16:49:21 +0200

    added pytube_extensions.py
    for functions relating to pytubefix

---
+++ src/core/utilities/pytubefix_extensions.py
@@ -0,0 +1,14 @@
+from typing import Union, cast
+
+from pytubefix import YouTube, Stream
+from pytubefix.async_youtube import AsyncYouTube
+
+# Temporary fix for https://github.com/JuanBindez/pytubefix/issues/529
+async def stream_default_filename(stream: Stream) -> str:
+    stream_youtube: Union[YouTube, AsyncYouTube] = cast(Union[YouTube, AsyncYouTube], stream._monostate.youtube)
+    youtube_title = await stream_youtube.title() if isinstance(stream_youtube, AsyncYouTube) else stream_youtube.title
+
+    return f"{youtube_title}.{stream.subtype}"
+
+def stream_repr(stream: Stream):
+    return f"subtype: {stream.subtype}, fps: {stream.fps if hasattr(stream, "fps") else "0"}, resolution: {stream.resolution}, bitrate: {stream.bitrate / 1000 if stream.bitrate != None else "?"}kbps, file size (MB): {stream.filesize_mb}, includes video track: {stream.includes_video_track}, includes audio track: {stream.includes_audio_track}, video codec: {stream.video_codec}, audio codec: {stream.audio_codec}"
