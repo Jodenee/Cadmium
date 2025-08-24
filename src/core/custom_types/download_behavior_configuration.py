5672f9af58ee3bc54f47497a226ba35dc0c2546b
Jodenee <81998397+Jodenee@users.noreply.github.com>
2025-08-24 16:41:05 +0200

    update download_behavior_configuration.py
    reworked conversion settings

--- src/core/custom_types/download_behavior_configuration.py
+++ src/core/custom_types/download_behavior_configuration.py
@@ -2,3 +2,13 @@ from typing import TypedDict
 
 class DownloadBehaviorConfiguration(TypedDict):
     skip_existing_files: bool
+
+    convert_video_downloads: bool
+    convert_video_only_downloads: bool
+    convert_audio_only_downloads: bool
+    merge_best_of_both_downloads_into_one_file: bool
+
+    convert_video_downloads_to: str
+    convert_video_only_downloads_to: str
+    convert_audio_only_downloads_to: str
+    best_of_both_merged_file_format: str
