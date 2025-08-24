e2c0c041a4fc76d023c9dc12843edc0dde91b975
Jodenee <81998397+Jodenee@users.noreply.github.com>
2025-08-24 16:42:19 +0200

    update quality_of_life_configuration.py
    move conversion config

--- src/core/custom_types/quality_of_life_configuration.py
+++ src/core/custom_types/quality_of_life_configuration.py
@@ -5,8 +5,5 @@ from .custom_download_location_configuration import CustomDownloadLocationConfig
 class QualityOfLifeConfiguration(TypedDict):
     custom_download_locations: CustomDownloadLocationConfiguration
     put_playlist_videos_in_folder: bool
-    convert_video_only_downloads_to_mp4: bool
-    convert_audio_only_downloads_to_mp3: bool
-    combine_best_of_both_downloads_into_one_file: bool
     display_chosen_stream_on_start_of_download: bool
     clear_temporary_files_before_exiting: bool
