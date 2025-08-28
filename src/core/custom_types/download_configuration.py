a011d30ff0294af7508a978c6bef0774fbc5a767
Jodenee <81998397+Jodenee@users.noreply.github.com>
2025-08-28 18:19:16 +0200

    update download_configuration.py
    rename custom download path to download location override

--- src/core/custom_types/download_configuration.py
+++ src/core/custom_types/download_configuration.py
@@ -2,6 +2,6 @@ from pathlib import Path
 from typing import Optional, TypedDict
 
 class DownloadConfiguration(TypedDict):
-    use_custom_download_location: bool
-    custom_download_location: Optional[str]
+    use_download_location_override: bool
+    download_location_override: Optional[str]
     default_download_location: Path
