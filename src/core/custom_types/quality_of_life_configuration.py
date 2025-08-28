bcf1dc92d5091ed66388f34560b52a10102a42cf
Jodenee <81998397+Jodenee@users.noreply.github.com>
2025-08-28 18:29:38 +0200

    update quality_of_life_configuration.py

--- src/core/custom_types/quality_of_life_configuration.py
+++ src/core/custom_types/quality_of_life_configuration.py
@@ -1,9 +1,9 @@
 from typing import TypedDict
 
-from .custom_download_location_configuration import CustomDownloadLocationConfiguration
+from .download_location_overrides_configuration import DownloadLocationOverridesConfiguration
 
 class QualityOfLifeConfiguration(TypedDict):
-    custom_download_locations: CustomDownloadLocationConfiguration
+    download_location_overrides: DownloadLocationOverridesConfiguration
     put_playlist_videos_in_folder: bool
     display_chosen_stream_on_start_of_download: bool
     clear_temporary_files_before_exiting: bool
