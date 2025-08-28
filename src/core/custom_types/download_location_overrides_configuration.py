eb3713b06f4dff0f52dc226a792fb4f6746f0c30
Jodenee <81998397+Jodenee@users.noreply.github.com>
2025-08-28 18:20:24 +0200

    update download_location_overrides_configuration.py
    rename custom download location with download location override

--- src/core/custom_types/download_location_overrides_configuration.py
+++ src/core/custom_types/download_location_overrides_configuration.py
@@ -1,11 +1,13 @@
 from typing import TypedDict, Optional
 
-class CustomDownloadLocationConfiguration(TypedDict):
-    use_custom_video_download_location: bool
-    use_custom_audio_download_location: bool
-    use_custom_video_only_download_location: bool
-    use_custom_best_of_both_download_location: bool
-    custom_video_download_location: Optional[str]
-    custom_audio_download_location: Optional[str]
-    custom_video_only_download_location: Optional[str]
-    custom_best_of_both_download_location: Optional[str]
+class DownloadLocationOverridesConfiguration(TypedDict):
+    use_video_download_location_override: bool
+    use_audio_download_location_override: bool
+    use_video_only_download_location_override: bool
+    use_best_of_both_download_location_override: bool
+    use_custom_download_location_override: bool
+    video_download_location_override: Optional[str]
+    audio_download_location_override: Optional[str]
+    video_only_download_location_override: Optional[str]
+    best_of_both_download_location_override: Optional[str]
+    custom_download_location_override: Optional[str]
