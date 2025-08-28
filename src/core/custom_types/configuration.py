259106b125c81e1698baa0be2f4eeb33ca5c0ed9
Jodenee <81998397+Jodenee@users.noreply.github.com>
2025-08-28 18:17:23 +0200

    update configuration.py
    add external dependency configuration

--- src/core/custom_types/configuration.py
+++ src/core/custom_types/configuration.py
@@ -4,9 +4,11 @@ from .quality_of_life_configuration import QualityOfLifeConfiguration
 from .warning_configuration import WarningConfiguration
 from .download_behavior_configuration import DownloadBehaviorConfiguration
 from .ui_configuration import UIConfiguration
+from .external_dependency_configuration import ExternalDependencyConfiguration
 
 class Configuration(TypedDict):
     download_behavior_configuration: DownloadBehaviorConfiguration
     quality_of_life_configuration: QualityOfLifeConfiguration
     warning_configuration: WarningConfiguration
     ui_configuration: UIConfiguration
+    external_dependency_configuration: ExternalDependencyConfiguration
