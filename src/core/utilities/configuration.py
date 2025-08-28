1bd13ca82395f1de1b154c98622f53ec2ff5aa6b
Jodenee <81998397+Jodenee@users.noreply.github.com>
2025-08-28 18:54:56 +0200

    update configuration.py
    update default config

--- src/core/utilities/configuration.py
+++ src/core/utilities/configuration.py
@@ -7,26 +7,32 @@ from ..custom_types import Configuration
 default_configuration: Configuration = {
     "download_behavior_configuration": {
         "skip_existing_files": True,
+
         "convert_video_downloads": False,
         "convert_video_only_downloads": False,
         "convert_audio_only_downloads": False,
         "merge_best_of_both_downloads_into_one_file": False,
+        "convert_custom_downloads": False,
+        
         "convert_video_downloads_to": "mp4",
         "convert_video_only_downloads_to": "mp4",
         "convert_audio_only_downloads_to": "mp3",
-        "best_of_both_merged_file_format": "mp4"
+        "best_of_both_merged_file_format": "mp4",
+        "convert_custom_downloads_to": "mp4"
     },
     "quality_of_life_configuration": {
-        "custom_download_locations": {
-            "use_custom_video_download_location": False,
-            "use_custom_video_only_download_location": False,
-            "use_custom_audio_download_location": False,
-            "use_custom_best_of_both_download_location": False,
+        "download_location_overrides": {
+            "use_video_download_location_override": False,
+            "use_video_only_download_location_override": False,
+            "use_audio_download_location_override": False,
+            "use_best_of_both_download_location_override": False,
+            "use_custom_download_location_override": False,
             
-            "custom_video_download_location": "",
-            "custom_video_only_download_location": "",
-            "custom_audio_download_location": "",
-            "custom_best_of_both_download_location": ""
+            "video_download_location_override": "",
+            "video_only_download_location_override": "",
+            "audio_download_location_override": "",
+            "best_of_both_download_location_override": "",
+            "custom_download_location_override": ""
         },
         "put_playlist_videos_in_folder": True,
         "display_chosen_stream_on_start_of_download": True,
@@ -40,6 +46,12 @@ default_configuration: Configuration = {
         "custom_download_bar_colour": None,
         "custom_convert_bar_colour": None,
         "custom_clear_directory_bar_colour": None
+    },
+    "external_dependency_configuration": {
+        "FFmpeg": {
+            "try_find_ffmpeg_path_automatically": True,
+            "ffmpeg_executable_path": ""
+        }
     }
 }
 
