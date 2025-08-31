262d4dc3d1e73f995c97edb8c609c460540ddf41
Jodenee <81998397+Jodenee@users.noreply.github.com>
2025-08-31 11:34:30 +0200

    add channel_download_result.py

---
+++ src/core/custom_types/channel_download_result.py
@@ -0,0 +1,11 @@
+from typing import List, Optional, TypedDict
+
+from pathlib import Path
+
+class ChannelDownloadResult(TypedDict):
+    success: bool
+    channel_name: str
+    download_directory_path: Optional[Path]
+    failed_downloads: List[str]
+    error_message: Optional[str]
+    
