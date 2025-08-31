555f5041e12c462fc9551ef1415efcc4451efe63
Jodenee <81998397+Jodenee@users.noreply.github.com>
2025-08-31 13:43:32 +0200

    update console.py
    fix spacing issue

--- src/core/utilities/console.py
+++ src/core/utilities/console.py
@@ -1,4 +1,4 @@
 from typing import Generic, Optional, Tuple, TypeVar
 
-def spaced_print(*objects, sep: Optional[str] = None, flush: bool = False) -> None:
-    print(*("\n", *objects), sep=sep, flush=flush)
+def spaced_print(*objects, sep: Optional[str] = "", flush: bool = False, end: Optional[str] = None) -> None:
+    print(*("\n", *objects), sep=sep, flush=flush, end=end)
