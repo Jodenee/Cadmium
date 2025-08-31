e1b53c582ba59cedfdcf34fdaed7e801f70e4b59
Jodenee <81998397+Jodenee@users.noreply.github.com>
2025-08-31 11:36:16 +0200

    update quality_of_life_configuration.py
    add config to put channel videos and custom stream downloads into their own folder for more orginization

--- src/core/custom_types/quality_of_life_configuration.py
+++ src/core/custom_types/quality_of_life_configuration.py
@@ -5,5 +5,7 @@ from .download_location_overrides_configuration import DownloadLocationOverrides
 class QualityOfLifeConfiguration(TypedDict):
     download_location_overrides: DownloadLocationOverridesConfiguration
     put_playlist_videos_in_folder: bool
+    put_channel_videos_in_folder: bool
+    put_custom_streams_in_folder: bool
     display_chosen_stream_on_start_of_download: bool
     clear_temporary_files_before_exiting: bool
