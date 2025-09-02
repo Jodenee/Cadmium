5a3b67d73f61e09c03a1db3f228b5d5a958a86f1
Jodenee <81998397+Jodenee@users.noreply.github.com>
2025-09-02 19:47:24 +0200

    update playlist_download_result.py
    failed_downloads should be a list of FailedDownloadInformation dicts

--- src/core/custom_types/playlist_download_result.py
+++ src/core/custom_types/playlist_download_result.py
@@ -1,11 +1,11 @@
-from typing import List, Optional, TypedDict
-
+from typing import List, Optional, Tuple, TypedDict
 from pathlib import Path
+
+from .failed_download_information import FailedDownloadInformation
 
 class PlaylistDownloadResult(TypedDict):
     success: bool
     playlist_name: str
     download_directory_path: Optional[Path]
-    failed_downloads: List[str]
-    error_message: Optional[str]
+    failed_downloads: List[FailedDownloadInformation]
     
