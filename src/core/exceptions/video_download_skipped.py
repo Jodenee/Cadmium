f32f7e0643dc99a322fa3757bff1dfd34b37d1e1
Jodenee <81998397+Jodenee@users.noreply.github.com>
2025-09-02 21:24:21 +0200

    update video_download_skipped.py
    videos can be skipped for more reasons later

--- src/core/exceptions/video_download_skipped.py
+++ src/core/exceptions/video_download_skipped.py
@@ -1,5 +1,3 @@
-from pathlib import Path
-
 class VideoDownloadSkipped(Exception):
-    def __init__(self, youtube_video_title: str, path: Path) -> None:
-        super().__init__(f"YouTube video: ({youtube_video_title}) has not been downloaded as it already exists in ({path}).")
+    def __init__(self, reason: str) -> None:
+        super().__init__(reason)
