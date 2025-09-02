be775fc569894dff385319b93bb800469e31060b
Jodenee <81998397+Jodenee@users.noreply.github.com>
2025-09-02 21:26:10 +0200

    update console.py
    added helper function to print failed downloads nicly for the user!

--- src/core/utilities/console.py
+++ src/core/utilities/console.py
@@ -1,4 +1,14 @@
-from typing import Generic, Optional, Tuple, TypeVar
+from typing import Generic, List, Optional, Tuple, TypeVar
+
+from core.custom_types.failed_download_information import FailedDownloadInformation
 
 def spaced_print(*objects, sep: Optional[str] = "", flush: bool = False, end: Optional[str] = None) -> None:
     print(*("\n", *objects), sep=sep, flush=flush, end=end)
+
+def print_failed_downloads(failed_downloads: List[FailedDownloadInformation]):
+    failed_download_info = [
+        f"{download_failure_info['youtube_video_title']} ({download_failure_info["youtube_video_url"]})\nReason: \"{download_failure_info["error_message"]}\"" 
+        for download_failure_info in failed_downloads 
+    ]
+
+    spaced_print(str.join("\n\n", failed_download_info))
