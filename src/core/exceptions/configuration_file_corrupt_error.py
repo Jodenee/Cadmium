b45f1a6a1771ff5f84469711159306e74c08fef5
Jodenee <81998397+Jodenee@users.noreply.github.com>
2025-09-02 21:33:22 +0200

    removed

--- src/core/exceptions/configuration_file_corrupt_error.py
+++
@@ -1,5 +0,0 @@
-from typing import Type
-
-class ConfigurationFileCorruptError(Exception):
-    def __init__(self) -> None:
-        super().__init__("Configuration file could not be loaded, try deleting the configuration file and re-running the program.")
