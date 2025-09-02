2b9bbddb140d8503da9edab21f11e95744952786
Jodenee <81998397+Jodenee@users.noreply.github.com>
2025-09-02 21:22:43 +0200

    add trailing newline
    so pro!

--- src/core/exceptions/invalid_youtube_url_error.py
+++ src/core/exceptions/invalid_youtube_url_error.py
@@ -1,3 +1,3 @@
 class InvalidYoutubeURLError(Exception):
     def __init__(self, given_url: str) -> None:
-        super().__init__(f"URL: ({given_url}) is not a valid youtube url.")
+        super().__init__(f"({given_url}) is not a valid youtube url.")
