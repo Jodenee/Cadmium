1374f292ac01dc5cbb407a7d49945893eb77bfc4
Jodenee <81998397+Jodenee@users.noreply.github.com>
2025-09-06 16:24:44 +0200

    update download_location_overrides_configuration.py
    fix typo

--- src/core/custom_types/download_location_overrides_configuration.py
+++ src/core/custom_types/download_location_overrides_configuration.py
@@ -2,12 +2,12 @@ from typing import TypedDict, Optional
 
 class DownloadLocationOverridesConfiguration(TypedDict):
     use_video_download_location_override: bool
-    use_audio_download_location_override: bool
+    use_audio_only_download_location_override: bool
     use_video_only_download_location_override: bool
     use_best_of_both_download_location_override: bool
     use_custom_download_location_override: bool
     video_download_location_override: Optional[str]
-    audio_download_location_override: Optional[str]
+    audio_only_download_location_override: Optional[str]
     video_only_download_location_override: Optional[str]
     best_of_both_download_location_override: Optional[str]
     custom_download_location_override: Optional[str]
