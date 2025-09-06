867e16f218d9903e2bb32f352205c10c8c5c0a5d
Jodenee <81998397+Jodenee@users.noreply.github.com>
2025-09-06 16:28:14 +0200

    update pytubefix_extensions.py
    keep spacing consistant

--- src/core/utilities/pytubefix_extensions.py
+++ src/core/utilities/pytubefix_extensions.py
@@ -10,5 +10,6 @@ async def stream_default_filename(stream: Stream) -> str:
 
     return f"{youtube_title}.{stream.subtype}"
 
+
 def stream_repr(stream: Stream) -> str:
     return f"subtype: {stream.subtype}, fps: {stream.fps if hasattr(stream, 'fps') else '0'}, resolution: {stream.resolution}, bitrate: {stream.bitrate / 1000 if stream.bitrate != None else '?'}kbps, file size (MB): {stream.filesize_mb}, includes video track: {stream.includes_video_track}, includes audio track: {stream.includes_audio_track}, video codec: {stream.video_codec}, audio codec: {stream.audio_codec}"
