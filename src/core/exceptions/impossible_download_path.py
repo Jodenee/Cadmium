e34b04e0ca40ab52a2522d2945f4f9a6de5df1ce
Jodenee <81998397+Jodenee@users.noreply.github.com>
2025-09-12 12:19:26 +0200

    update impossible_download_path.py
    update error message

--- src/core/exceptions/impossible_download_path.py
+++ src/core/exceptions/impossible_download_path.py
@@ -2,4 +2,4 @@ from pathlib import Path
 
 class ImpossibleDownloadPath(Exception):
     def __init__(self, path: Path) -> None:
-        super().__init__(f"Cannot download media to ({path}) due to hitting the system's path length limit. Try shortening the name of the folder or picking another.")
+        super().__init__(f"Cannot download media to ({path}) due to hitting the operating system's path length limit. Try shortening the name of the folder or picking another.")
