1e7d111db8880488248ba6f21662342f4cc4e38e
Jodenee <81998397+Jodenee@users.noreply.github.com>
2026-02-12 23:28:12 +0100

    refactor(pytubefix_extensions): refactored stream_repr function to be much more readable and easier to add to later + added pytubefix issue link to temp fix func

--- src/core/utilities/pytubefix_extensions.py
+++ src/core/utilities/pytubefix_extensions.py
@@ -1,11 +1,26 @@
-from typing import Union, cast
-
 from pytubefix import YouTube, Stream, Channel
 
 def stream_repr(stream: Stream) -> str:
-    return f"subtype: {stream.subtype}, fps: {stream.fps if hasattr(stream, 'fps') else '0'}, resolution: {stream.resolution}, bitrate: {stream.bitrate / 1000 if stream.bitrate != None else '?'}kbps, file size (MB): {stream.filesize_mb}, includes video track: {stream.includes_video_track}, includes audio track: {stream.includes_audio_track}, video codec: {stream.video_codec}, audio codec: {stream.audio_codec}"
+    separator = " | "
+    fps = stream.fps if hasattr(stream, 'fps') else 'N/A'
+    bitrate = f"{stream.bitrate / 1000}kbps" if stream.bitrate != None else 'N/A'
 
-# Temporary fix for channel video_urls generator returning YoutubeVideo instances
+    information_text = str.join(separator, (
+        f"subtype: {stream.subtype}",
+        f"has video track: {stream.includes_video_track}",
+        f"has audio track: {stream.includes_audio_track}",
+        f"fps: {fps}",                                    
+        f"resolution: {stream.resolution}",              
+        f"bitrate: {bitrate}",              
+        f"file size (MB): {stream.filesize_mb}",
+        f"video codec: {stream.video_codec}",
+        f"audio codec: {stream.audio_codec}"
+    ))
+
+    return information_text
+    
+
+# Temporary fix for channel video_urls generator returning YoutubeVideo instances (https://github.com/JuanBindez/pytubefix/issues/577)
 def get_channel_video_urls(channel: Channel):
     video: YouTube
     for video in channel.video_urls:
