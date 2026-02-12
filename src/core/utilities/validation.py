c331ee3bc870ca3fcf085a3953b4d8d47c63df72
Jodenee <81998397+Jodenee@users.noreply.github.com>
2026-02-12 22:54:36 +0100

    style: made error raise a bit more readable

--- src/core/utilities/validation.py
+++ src/core/utilities/validation.py
@@ -12,6 +12,9 @@ def ensure_can_use_ffmpeg(
     if not should_convert: return
 
     if ffmpeg_executable_path == None:
-        raise InvalidConfigurationError("FFmpeg", "cannot convert videos without FFmpeg, please enable \"try_find_ffmpeg_path_automatically\" or manually set the path to the executable using the \"ffmpeg_executable_path\" setting")
+        raise InvalidConfigurationError(
+            "FFmpeg", 
+            "cannot convert videos without FFmpeg, please enable \"try_find_ffmpeg_path_automatically\" or manually set the path to the executable using the \"ffmpeg_executable_path\" setting"
+        )
     elif file_extension == None or len(file_extension.strip()) == 0:
         raise InvalidConfigurationError(file_extension_configuration_name, "is empty")
