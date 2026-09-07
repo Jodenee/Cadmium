13d524e06e2cbf4accc4e2ece2ad1dedd05b99a1
Jodenee <81998397+Jodenee@users.noreply.github.com>
2026-09-07 09:02:07 +0200

    feat: improve displaying of stream information by avoiding None being printed and instead using more well understood N/A

--- src/core/utilities/pytubefix_extensions.py
+++ src/core/utilities/pytubefix_extensions.py
@@ -15,17 +15,19 @@ def stream_repr(stream: Stream) -> str:
     fps = stream.fps if hasattr(stream, 'fps') else 'N/A'
     resolution = stream.resolution if hasattr(stream, 'resolution') and stream.resolution is not None else 'N/A'
     bitrate = f"{stream.bitrate / 1000}kbps" if stream.bitrate is not None else 'N/A'
+    video_codec = stream.video_codec if stream.video_codec is not None else 'N/A'
+    audio_codec = stream.audio_codec if stream.audio_codec is not None else 'N/A'
 
     information_text = str.join(separator, (
         f"subtype: {stream.subtype}",
         f"has video track: {stream.includes_video_track}",
         f"has audio track: {stream.includes_audio_track}",
-        f"fps: {fps}",                                    
-        f"resolution: {resolution}",              
-        f"bitrate: {bitrate}",              
+        f"fps: {fps}",                      
+        f"resolution: {resolution}",
+        f"bitrate: {bitrate}",
         f"file size in MB: {stream.filesize_mb}",
-        f"video codec: {stream.video_codec}",
-        f"audio codec: {stream.audio_codec}"
+        f"video codec: {video_codec}",
+        f"audio codec: {audio_codec}"
     ))
 
     return information_text
